3ae6388c223c3a4706e2044b30c6a69f922871a5
TolyaSideltcev <81379982+TolyaSideltcev@users.noreply.github.com>
2024-03-24 00:20:53 +0700

    Add files via upload

---
+++ probe/For.py
@@ -0,0 +1,6 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars)):
+    print('Я езжу на автомабиле марки ', cars[i])
+    cars_count += 10
+    print(cars_count)
