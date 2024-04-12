4d23a24ea2384fd4a3f9b01eb3df338a5bf794e7
TolyaSideltcev <81379982+TolyaSideltcev@users.noreply.github.com>
2024-04-12 23:07:58 +0700

    Update def test.py

--- def test.py
+++ def test.py
@@ -1,6 +1,11 @@
-def test(a = 1, b = 2):
+def test():
+    a = 1
+    b = 2
     print(a, b)
-def test2(a = 1, b = 2, c = 3):
+def test2():
+    a = 1
+    b = 2
+    c = 3
     print(a, b, c)
 
 test()
